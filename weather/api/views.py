7b55f1789484272ea9b52f5194107d45e587a657
Steve Helms <steve@cadenrock.com>
2019-04-09 14:16:56 +0000

    Added Create to temperature list view.

--- weather/api/views.py
+++ weather/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from .models import Temperature
 from .serializers import TemperatureSerializer
 
-class TemperatureList(generics.ListAPIView):
+class TemperatureList(generics.ListCreateAPIView):
     queryset = Temperature.objects.all()
     serializer_class = TemperatureSerializer
     
